a5bef4eeb7382e4c66744a1412a6cf0bf970ac06
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-11 16:32:36 +0800

    8.1 created Authors component

---
+++ src/components/Authors.jsx
@@ -0,0 +1,27 @@
+export default function Authors({ show, authors }) {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>Authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th>name</th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map(a => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
