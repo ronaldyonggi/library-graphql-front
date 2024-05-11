6ee25fd82571958dbaa0fb7bd8d66d01a740e04c
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-11 16:32:24 +0800

    8.1 initial app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+import Authors from './components/Authors';
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from './queries';
 
-function App() {
+export default function App() {
+  const [page, setPage] = useState('authors');
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
   return (
-    <>
-    </>
-  )
-}
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>Authors</button>
+        <button onClick={() => setPage('books')}>Books</button>
+        <button onClick={() => setPage('add')}>Add new book</button>
+      </div>
 
-export default App
+      <Authors show={page === "authors"} authors={result.data.allAuthors} />
+    </div>
+  );
+}
