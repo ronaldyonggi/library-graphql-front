360a92ceca936173ee0ac73383cc03ea48edb461
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-15 13:12:20 +0800

    8.11 define query EDIT_AUTHOR

--- src/queries.js
+++ src/queries.js
@@ -30,11 +30,26 @@ export const CREATE_BOOK = gql`
     $published: Int!
     $genres: [String!]!
   ) {
-    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
       title
       author
       published
       genres
+    }
+  }
+`;
+
+export const EDIT_AUTHOR = gql`
+  #graphql
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
     }
   }
 `;
