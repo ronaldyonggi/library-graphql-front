f25f1315b555d2dd57300c90647769689ae87ff3
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-15 13:12:53 +0800

    8.11 include SetAuthorBirth component

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,40 +1,45 @@
-import { useQuery } from "@apollo/client";
-import { ALL_AUTHORS } from "../queries";
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from '../queries';
+import SetAuthorBirth from './SetAuthorBirth';
 
 export default function Authors({ show }) {
-  const {loading, error, data} = useQuery(ALL_AUTHORS, {
-    skip: !show
-  })
+  const { loading, error, data } = useQuery(ALL_AUTHORS, {
+    skip: !show,
+  });
 
   if (!show) {
     return null;
   }
 
   if (loading) {
-    return <div>loading...</div>
+    return <div>loading...</div>;
   }
 
   const authors = data.allAuthors;
 
   return (
     <div>
-      <h2>Authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>name</th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map(a => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+      <div>
+        <h2>Authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>name</th>
+              <th>born</th>
+              <th>books</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {authors.map((a) => (
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <SetAuthorBirth />
     </div>
   );
 }
