f06392ea11a44c8cdff53d802b693d06eba1753f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-08 21:42:01 +0800

    LOGIN mutation returns username

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -36,6 +36,7 @@ export const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
       value
+      username
       favoriteGenre
     }
   }
